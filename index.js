a325c632e406dda0dc75d2d902e10bfee19fd8c0
Daewony <alexnori.com@gmail.com>
2023-08-03 00:21:46 +0900

    [Feat] esc 클릭시 모달창 닫음

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const containerEl = document.querySelector(".container");
-
 const btnEl = document.querySelector(".container-btn");
-
 const popupContainerEl = document.querySelector(".popup-container");
-
 const closeIconEl = document.querySelector(".close-icon");
 
 btnEl.addEventListener("click", () => {
@@ -14,4 +11,13 @@ btnEl.addEventListener("click", () => {
 closeIconEl.addEventListener("click", () => {
     containerEl.classList.remove("active");
     popupContainerEl.classList.add("active");
+    document.getElementById("email-input").value = "";
 });
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        containerEl.classList.remove("active");
+        popupContainerEl.classList.add("active");
+        document.getElementById("email-input").value = "";
+    }
+})
